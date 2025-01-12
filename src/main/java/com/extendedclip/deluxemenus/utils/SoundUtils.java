63bde0dd31b5439112ce96ce419dda09eed2e482
MemencioPerez <memencio.perez.2004@gmail.com>
2025-01-12 04:00:53 -0400

    Catch only the specific types of exceptions that may be thrown when attempting to access the Sound#valueOf method through Reflection

--- src/main/java/com/extendedclip/deluxemenus/utils/SoundUtils.java
+++ src/main/java/com/extendedclip/deluxemenus/utils/SoundUtils.java
@@ -2,6 +2,7 @@ package com.extendedclip.deluxemenus.utils;
 
 import org.bukkit.Sound;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class SoundUtils {
@@ -12,7 +13,7 @@ public class SoundUtils {
             // This fixes java.lang.IncompatibleClassChangeError when trying to use versions prior to 1.21.3.
             Method valueOfMethod = Class.forName("org.bukkit.Sound").getMethod("valueOf", String.class);
             return (Sound) valueOfMethod.invoke(null, name);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             // Use the Sound#valueOf method if Reflection fails.
             return Sound.valueOf(name);
         }

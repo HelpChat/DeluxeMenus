797fd264252abe742899fab234c9208d95e3e7ab
Kqliber <36670020+Kqliber@users.noreply.github.com>
2025-07-17 18:26:12 +0100

    change login event to join event (#223)
    Co-authored-by: Ruben <52609756+BlitzOffline@users.noreply.github.com>

--- src/main/java/com/extendedclip/deluxemenus/dupe/DupeFixer.java
+++ src/main/java/com/extendedclip/deluxemenus/dupe/DupeFixer.java
@@ -6,7 +6,7 @@ import com.extendedclip.deluxemenus.utils.DebugLevel;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -54,7 +54,7 @@ public class DupeFixer extends Listener {
     }
 
     @EventHandler
-    private void onLogin(@NotNull final PlayerLoginEvent event) {
+    private void onLogin(@NotNull final PlayerJoinEvent event) {
         plugin.getServer().getScheduler().runTaskLater(
                 plugin,
                 () -> {

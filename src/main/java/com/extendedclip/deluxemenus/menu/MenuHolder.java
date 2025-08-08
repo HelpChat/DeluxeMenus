1fa0573a28b3513431d8e9259d2eb87d2683ed8d
FakE <mohammadsalehsalehi.mss@gmail.com>
2025-08-08 20:25:30 +0330

    not used, not needed

--- src/main/java/com/extendedclip/deluxemenus/menu/MenuHolder.java
+++ src/main/java/com/extendedclip/deluxemenus/menu/MenuHolder.java
@@ -35,7 +35,6 @@ public class MenuHolder implements InventoryHolder {
     private boolean updating;
     private boolean parsePlaceholdersInArguments;
     private boolean parsePlaceholdersAfterArguments;
-    private boolean enableBypassPerm;
     private Map<String, String> typedArgs;
 
     public MenuHolder(final @NotNull DeluxeMenus plugin, final @NotNull Player viewer) {
@@ -344,17 +343,21 @@ public class MenuHolder implements InventoryHolder {
         this.typedArgs = typedArgs;
     }
 
-    public void parsePlaceholdersInArguments(final boolean parsePlaceholdersInArguments) {this.parsePlaceholdersInArguments = parsePlaceholdersInArguments;}
+    public void parsePlaceholdersInArguments(final boolean parsePlaceholdersInArguments) {
+        this.parsePlaceholdersInArguments = parsePlaceholdersInArguments;
+    }
 
-    public void parsePlaceholdersAfterArguments(final boolean parsePlaceholdersAfterArguments) {this.parsePlaceholdersAfterArguments = parsePlaceholdersAfterArguments;}
+    public void parsePlaceholdersAfterArguments(final boolean parsePlaceholdersAfterArguments) {
+        this.parsePlaceholdersAfterArguments = parsePlaceholdersAfterArguments;
+    }
 
-    public void enableBypassPerm(final boolean enableBypassPerm) {this.enableBypassPerm = enableBypassPerm;}
+    public boolean parsePlaceholdersInArguments() {
+        return parsePlaceholdersInArguments;
+    }
 
-    public boolean parsePlaceholdersInArguments() {return parsePlaceholdersInArguments;}
-
-    public boolean parsePlaceholdersAfterArguments() {return parsePlaceholdersAfterArguments;}
-
-    public boolean enableBypassPerm() {return enableBypassPerm;}
+    public boolean parsePlaceholdersAfterArguments() {
+        return parsePlaceholdersAfterArguments;
+    }
 
     public void setPlaceholderPlayer(Player placeholderPlayer) {
         this.placeholderPlayer = placeholderPlayer;

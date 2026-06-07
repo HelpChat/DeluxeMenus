2d31f7a3750ee903461d42423242e945230ef914
charlyg31 <charlyg31@github.com>
2026-06-07 12:29:19 +0200

    fix: correct version numbers for craftbukkit package check

--- src/main/java/com/extendedclip/deluxemenus/utils/VersionHelper.java
+++ src/main/java/com/extendedclip/deluxemenus/utils/VersionHelper.java
@@ -217,7 +217,7 @@ public final class VersionHelper {
      * @return The craft class.
      */
     public static Class<?> getCraftClass(@NotNull final String name) throws ClassNotFoundException {
-        if (CURRENT_VERSION >= 26_1 || IS_PAPER && CURRENT_VERSION >= 1_20_5) {
+        if (CURRENT_VERSION >= 2610 || (IS_PAPER && CURRENT_VERSION >= 12050)) {
             try {
                 return Class.forName("org.bukkit.craftbukkit." + name);
             } catch (ClassNotFoundException ignored) { }

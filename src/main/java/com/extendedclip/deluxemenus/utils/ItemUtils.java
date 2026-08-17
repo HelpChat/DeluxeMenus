6be2990d250f1d83bff90337c8b50dac1c651503
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2026-08-17 22:58:09 +0300

    Remove remaining TODOs

--- src/main/java/com/extendedclip/deluxemenus/utils/ItemUtils.java
+++ src/main/java/com/extendedclip/deluxemenus/utils/ItemUtils.java
@@ -105,7 +105,7 @@ public final class ItemUtils {
         final PotionMeta itemMeta = (PotionMeta) itemStack.getItemMeta();
 
         if (itemMeta != null) {
-            itemMeta.setBasePotionType(PotionType.WATER); // TODO: Blitz: Check if this works in 1.17.1 (seems that the setBasePotionType method was only added later)
+            itemMeta.setBasePotionType(PotionType.WATER);
             itemStack.setItemMeta(itemMeta);
         }
 
